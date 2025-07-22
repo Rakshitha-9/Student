01c4c045088ec65f1992a8ba2d654a46ab879681
Rakshitha-9 <maddelarakshitha3@gmail.com>
2025-07-22 23:05:32 +0530

    Update StudentDbContext.cs

--- Studentdetails/Data/StudentDbContext.cs
+++ Studentdetails/Data/StudentDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using StudentApp.Models; // ✅ Updated to match model
+using StudentApp.Models;
 
-namespace StudentApp.Data  // ✅ Do not use just "Student"
+namespace StudentApp.Data  
 {
     public class StudentDbContext : DbContext
     {
